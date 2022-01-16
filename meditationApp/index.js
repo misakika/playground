fd27f472043e701b9d42c60bb4bd674971360ab5
misaki <sasasaccc31315@gmail.com>
2022-01-16 10:55:57 +0900

    firstcommit

---
+++ meditationApp/index.js
@@ -0,0 +1,13 @@
+const app = () =>{
+  const song = document.querySelector(".song");
+  const play = document.querySelector(".paly");
+  const outline = document.querySelector(".moving-outline circle")
+  const video = document.querySelector(".vid-container viceo");
+
+  //sounds
+  const sounds = document.querySelector(".sound-picker-button");
+  //Time Display
+
+}
+
+app();
