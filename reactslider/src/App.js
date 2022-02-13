d4aa0f90fffc2683d9faae002b661d70a1ed8695
misaki <sasasaccc31315@gmail.com>
2022-02-13 21:34:00 +0900

    スクロール完成

--- reactslider/src/App.js
+++ reactslider/src/App.js
@@ -1,10 +1,34 @@
-
 import './App.css';
+import { motion } from "framer-motion";
+import { useRef, useEffect, useState } from "react";
+import images from "./images";
 
 function App() {
+  const [width, setWidth] = useState(0);
+  const carousel = useRef();
+
+  useEffect(() => {
+    console.log(carousel.current.scrollWidth , carousel.current.offsetWidth)
+    setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+  }, []);
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <motion.div ref={carousel} className="carousel" whileTap={{cursor: "grabbing"}} >
+        <motion.div
+          drag="x"
+          dragConstraints={{ right: 0, left: - width }}
+          className="inner-carousel"
+        >
+          {images.map(image => {
+            return (
+              <motion.div className="item" key={image}>
+                <img src={image} alt="" />
+              </motion.div>
+            )
+          })}
+        </motion.div>
+      </motion.div>
     </div>
   );
 }
---
+++ reactslider/src/images.js
@@ -0,0 +1,9 @@
+import image1 from "./img/pexels-charles-1851164 (2).jpg";
+import image2 from "./img/pexels-anna-shvets-4587998.jpg";
+import image3 from "./img/pexels-melissa-jansen-van-rensburg-1954515.jpg";
+import image4 from "./img/pexels-dominika-roseclay-4490129.jpg";
+import image5 from "./img/pexels-vanserline-vandenberg-1619690.jpg";
+import image6 from "./img/pexels-steshka-willems-1390361.jpg";
+import image7 from "./img/pexels-anna-shvets-4587991.jpg";
+
+export default [image1, image2, image3, image4, image5, image6, image7]
